74c7b63a727488bfa97c22ad9ada82e0e9b12fb2
Amirhossein Alibakhshi <amir78729@gmail.com>
2020-07-27 03:38:04 +0430

    Add files via upload

---
+++ phase1/prj/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "C:/Users/Asus/Desktop/phase 1 no assign/Phase 1/prj/ParityErrorChecker_isim_beh.exe" -prj "C:/Users/Asus/Desktop/phase 1 no assign/Phase 1/prj/ParityErrorChecker_beh.prj" "work.ParityErrorChecker" "work.glbl" 
